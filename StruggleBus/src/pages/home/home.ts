930bef4462a538def54278d30afae555e8d97590
Nancy Yao <nancy.yao2@gmail.com>
2018-04-11 17:18:25 -0500

    Adding names to database

--- StruggleBus/src/pages/home/home.ts
+++ StruggleBus/src/pages/home/home.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { DatabaseProvider, Quarter, Class } from '../../providers/database/database'
-
+import { DatabaseProvider, Quarter, Class, Student } from '../../providers/database/database'
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 
 
 @Component({
@@ -19,19 +19,31 @@ export class HomePage {
 
 	quarterClasses: Observable< Class[] >;
 
+  peopleInterested: Observable< Student [] >;
+  peopleCollection: AngularFirestoreCollection<Student>;
 
 
 
   // constructor(public navCtrl: NavController,  private db: AngularFirestore) {
   constructor(public navCtrl: NavController, public databaseService: DatabaseProvider) {
   		this.items = this.databaseService.quarters;
-  		// this.classes = this.databaseService.classes;
-
   		this.quarter = this.databaseService.quarter;
-
   		this.quarterClasses = this.databaseService.quarterClasses;
+
+      this.peopleInterested = this.databaseService.peopleInterested;
+      this.peopleCollection = this.databaseService.peopleCollection;
+
+      console.log(this.peopleCollection);
 	}
 
+
+  addStudent(): void {
+
+    this.peopleCollection.add({
+      id: 3,
+      name: "Jackie D"
+    })
+  }
 
 
   ionViewWillLoad() {
--- StruggleBus/src/providers/database/database.ts
+++ StruggleBus/src/providers/database/database.ts
@@ -7,15 +7,18 @@ import { Observable } from 'rxjs/Observable';
 export interface Class {
 	id: number;
 	name: string;
-	people_interested: number[];
+	people_interested: Student[];
 }
-
 
 export interface Quarter {
 	q_name: string;
 	classes: Class[];
 }
 
+export interface Student {
+	id: number;
+	name: string;
+}
 
 @Injectable()
 
@@ -26,33 +29,41 @@ export class DatabaseProvider {
 	private quartersCollection: AngularFirestoreCollection<Quarter>;
 	quarters: Observable< Quarter[] >;
 
-
 	private quarterDoc: AngularFirestoreDocument<Quarter>;
 	quarter: Observable< Quarter >;
 
 	quarterClasses: Observable< Class[] >;
 
+	// For class eecs394 (doc)
+	private classDoc: AngularFirestoreDocument<Class>;
+	class: Observable< Class >;
+	peopleInterested: Observable< Student [] >;
+
+	private peopleCollection: AngularFirestoreCollection<Student>;
 
 
-// the actual database that's populated with data (THAT GETS UPDATED ALL THE TIME U)
+	// the actual database that's populated with data (THAT GETS UPDATED ALL THE TIME U)
   	constructor(private afs: AngularFirestore) {
     	console.log('Hello DatabaseProvider Provider');
     	this.quartersCollection = afs.collection<Quarter>('quarters');
     	this.quarters = this.quartersCollection.valueChanges();
 
-    	this.quarterDoc = afs.doc< Quarter >('quarters/2018_spring/classes/eecs394');
+    	this.quarterDoc = afs.doc< Quarter >('quarters/2018_spring');
     	this.quarter = this.quarterDoc.valueChanges();
 
     	this.quarterClasses = this.quarterDoc.collection< Class > ('classes').valueChanges();
-  	}
 
+			this.classDoc = afs.doc< Quarter >('quarters/2018_spring/classes/eecs394');
+			this.class = this.classDoc.valueChanges();
+
+			this.peopleCollection = this.classDoc.collection< Student >('people_interested');
+			this.peopleInterested = this.peopleCollection.valueChanges();
+
+  	}
 
   	addQuarter(quarter: Quarter) {
   		this.quartersCollection.add(quarter);
   	}
 
 
-		addStudent(student_in_class: Student_in_class) {
-			this.quartersCollection.add(student_in_class);
-		}
 }
