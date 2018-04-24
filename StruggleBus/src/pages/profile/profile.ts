25699bcfe1a2d73a8dc7fb601fad76ff2bb532e2
Khoa Truong <khoa.truong@u.northwestern.edu>
2018-04-24 18:59:24 -0500

    add profile page

---
+++ StruggleBus/src/pages/profile/profile.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { UserProvider } from '../../providers/user/user';
+import { DatabaseProvider } from '../../providers/database/database';
+
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html'
+})
+export class ProfilePage implements OnInit{
+
+	  userData: any;
+    userCourses: any[];
+    fbData: any;
+    quarter: string;
+
+  	constructor(public navCtrl: NavController, public navParams: NavParams, public userService: UserProvider, public db: DatabaseProvider) {
+      this.fbData = navParams.get('user');
+      this.quarter = navParams.get('quarter');
+  		// this.userData = userService.userData;
+  	}
+
+    ngOnInit(){
+      this.db.getUser(this.fbData.id, this.fbData.name)
+      .subscribe(user => {
+        this.userData = user;
+        this.userCourses = []; // important, list doesn't render without this line
+        let quarters = user['courses_interested']
+        for (let q in quarters) {
+          this.userCourses[q] = Object.keys(quarters[q])
+        }
+      })
+      console.log(this.fbData);
+    }
+  // logout() {
+  //   this.userService.logout(this.navCtrl);
+  // }
+}
